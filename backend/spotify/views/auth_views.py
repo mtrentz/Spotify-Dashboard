69c461fa7416e4a634a4413c5a0059efec8312ed
mtrentz <mateus.trentz@gmail.com>
2022-01-09 14:18:40 -0300

    clean comment on auth

--- backend/spotify/views/auth_views.py
+++ backend/spotify/views/auth_views.py
@@ -16,7 +16,6 @@ class AuthURLView(APIView):
     url = sp.auth_manager.get_authorize_url()
 
     def get(self, request):
-
         return Response({"Please Sign In": self.url})
 
 
@@ -30,8 +29,6 @@ class AuthTokenView(APIView):
         ),
     )
 
-    print("Got in Auth token view")
-
     def post(self, request):
         try:
             # Get code from params
@@ -40,24 +37,6 @@ class AuthTokenView(APIView):
             token_info = self.sp.auth_manager.get_access_token(code)
             # Save it to file cache
             self.sp.auth_manager.cache_handler.save_token_to_cache(token_info)
-            # Saves token to the database. Trying to make it more long lasting
-            # TODO: not 100% sure this is the right way to do this
-            # SpotifyAuthorizationTokens.objects.update_or_create(
-            #     access_token=token_info["access_token"],
-            #     refresh_token=token_info["refresh_token"],
-            #     expires_in=token_info["expires_in"],
-            #     expires_at=token_info["expires_at"],
-            #     scope=token_info["scope"],
-            #     token_type=token_info["token_type"],
-            # # )
-            # sp.auth_manager.cache_handler.save_token_to_cache(token_info)
-            # Also saves token to session
-            # request.session["token_info"] = token
-            # request.session.modified = True
-            # print(request.session["token_info"])
-            # cache = DjangoSessionCacheHandler(request)
-            # cache.cache_access_token(token)
-
         except Exception as e:
             return Response({"Error": {f"Error storing tokens {e}"}})
         # Don't return the tokens since the user (browser) shouldnt't have access to it
--- backend/spotify/views/query_views.py
+++ backend/spotify/views/query_views.py
@@ -12,23 +12,12 @@ import os
 class RecentlyPlayedView(APIView):
     load_dotenv()
     cache_path = os.path.join(settings.BASE_DIR, "spotify", ".auth-cache")
-    # sp = spotipy.Spotify(
-    #     auth_manager=SpotifyOAuth(cache_handler=CacheFileHandler(cache_path)),
-    # )
 
     def get(self, request):
         sp = spotipy.Spotify(
             auth_manager=SpotifyOAuth(cache_handler=CacheFileHandler(self.cache_path)),
         )
-        # cache = sp.auth_manager.cache_handler.get_cached_token()
-        # print(cache)
-        # print(request.session.get("token_info"))
-        # cache = DjangoSessionCacheHandler(request)
-        # token = cache.get_cached_token()
-        # print(token)
         print(sp.auth_manager.cache_handler.get_cached_token())
 
         tracks = sp.current_user_recently_played(limit=2)
-        # print(tracks)
         return Response(tracks)
-        # return Response({"Success": "Got top tracks"})
