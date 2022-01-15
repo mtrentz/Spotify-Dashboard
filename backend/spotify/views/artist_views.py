2317447efe19a4c35e96877b9b7f5d8daeb6006d
mtrentz <mateus.trentz@gmail.com>
2022-01-15 16:56:10 -0300

    qty param for top artists

--- backend/spotify/views/artist_views.py
+++ backend/spotify/views/artist_views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.exceptions import ParseError
 from ..serializers.artist_serializers import (
     TopArtistsSerializers,
 )
@@ -10,10 +11,20 @@ class TopPlayedArtists(ListAPIView):
     serializer_class = TopArtistsSerializers
 
     def get_queryset(self):
-        # TODO: Catch error range
+
+        # Defaults to 10
+        try:
+            qty = int(self.request.query_params.get("qty", 10))
+        except ValueError:
+            raise ParseError("qty must be an integer")
+        if qty < 0:
+            raise ParseError("qty must be positive")
+        if qty > 50:
+            raise ParseError("qty must be less than 50")
+
         items = Artists.objects.annotate(
             time_played=Sum("tracks__useractivity__ms_played")
-        ).order_by("-time_played")[:10]
+        ).order_by("-time_played")[:qty]
         queryset = []
         for item in items:
             queryset.append(
