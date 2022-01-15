8143142fdb82ed52b3640bb68caa295f589e5c43
mtrentz <mateus.trentz@gmail.com>
2022-01-15 13:49:01 -0300

    axios + dummy api call

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect } from "react";
 import SampleGraph from "../components/Charts/SampleGraph";
 import TimePlayedChart from "../components/Charts/TimePlayedChart";
 import UniqueAlbums from "../components/Charts/UniqueAlbums";
@@ -9,6 +10,33 @@ import TopArtists from "../components/Charts/TopArtists";
 import TopTracks from "../components/Charts/TopTracks";
 
 const Home = () => {
+  const axios = require("axios");
+
+  const instance = axios.create({
+    // baseURL: "http://localhost:8000/api/spotify",
+    baseURL: "https://jsonplaceholder.typicode.com/",
+    timeout: 1000,
+  });
+
+  // // console.log(api.get("/recent"));
+  // api.get("/recent").then((res) => {
+  //   console.log("YOYOYOYO");
+  //   console.log(res.data);
+  // });
+
+  useEffect(() => {
+    instance
+      .get("/posts/1")
+      .then((res) => {
+        console.log("YOYOYOYO");
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log("sadge");
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div>
       <SampleGraph />
