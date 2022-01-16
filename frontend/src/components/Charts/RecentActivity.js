e5e39b73d4505f3a44c47943dc38f02225e1157d
mtrentz <mateus.trentz@gmail.com>
2022-01-16 15:58:47 -0300

    header for activity

--- frontend/src/components/Charts/RecentActivity.js
+++ frontend/src/components/Charts/RecentActivity.js
@@ -59,6 +59,9 @@ const RecentActivity = () => {
 
   return (
     <div className="card mx-10" style={{ height: "calc(24rem + 10px)" }}>
+      <div class="card-header flex justify-between">
+        <h3 class="card-title">Your Recent Activity</h3>
+      </div>
       <div className="card-body card-body-scrollable card-body-scrollable-shadow">
         <div className="divide-y">
           {recentlyPlayedData.map((item, index) => (
