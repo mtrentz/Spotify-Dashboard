6b41867a58cdb754b5f4050bc6fe36d7637a9241
mtrentz <mateus.trentz@gmail.com>
2022-01-28 17:10:37 -0300

    dev environment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -190,6 +190,8 @@ APSCHEDULER_RUN_NOW_TIMEOUT = 300  # Seconds
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
+    "http://localhost:8081",
+    "http://127.0.0.1:8081",
 
 ]
 
