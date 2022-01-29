f5d7a682ddaa2ce7d178d27892e32d9164ce7f4a
mtrentz <mateus.trentz@gmail.com>
2022-01-29 00:19:05 -0300

    docker compose stdin

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <footer class="footer footer-transparent d-print-none">
       <div class="container-xl">
-        <div class="row text-center align-items-center flex-row-reverse">
-          <div class="col-lg-auto ms-lg-auto">
+        <div class="row text-center align-items-center flex-row-reverse justify-center">
+          <div class="col-lg-auto">
             <ul class="list-inline list-inline-dots mb-0">
               <li class="list-inline-item">
                 <a
